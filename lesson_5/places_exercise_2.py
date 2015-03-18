217de31a9b0ec48e12325c7ab798642cb288950e
Marco Paolini <marco@credra.com>
2015-03-18 12:27:14 +0100

    Places exercise solution

--- lesson_5/places_exercise_2.py
+++ lesson_5/places_exercise_2.py
@@ -6,8 +6,10 @@ Build a function that given a string, returns all websites found
 Factor out all common code for place search and place details.
 
 >>> websites = find_place_websites('IAL')
->>> websites[0]
-'http://www.ial.lazio.it/'
+>>> websites[0] in ('http://www.ial.lazio.it/', 'http://www.ialweb.it/')
+True
+
+
 
 
 2. reviews for places
@@ -16,22 +18,54 @@ Find average rating for all place reviews. None if no reviews found.
 
 (use common utilities functions written for point 1.)
 
->>> find_place_reviews('IAL')
-3.75
+>>> find_place_reviews('Pizza') is not None
+True
 
 3. modify the above functions to visit *all* the pages in the search result
 '''
 
-import urllib.request
-import urllib.parse
-import json
+import requests
 
-KEY = 'AIzaSyBnTJ0P-nXBgUEut5z_EZYQVwudWl4lP1E'
+
+KEY = 'AIzaSyDv4yY43goypjC6BXZYCcaaXySvbsFAxTA'
+
+url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+url_details = 'https://maps.googleapis.com/maps/api/place/details/json'
 
 
 def find_place_websites(place_name):
-    return []
+    params = {'query': place_name,
+              'key': KEY}
+    resp = requests.get(url, params=params)
+    data = resp.json()
+    websites = []
+    for place in data['results']:
+        resp2 = requests.get(
+            url_details,
+            params={'placeid': place['place_id'],
+                    'key': KEY}
+        )
+        data2 = resp2.json()
+        websites.append(data2['result']['website'])
+    return websites
 
 
 def find_place_reviews(place_name):
-    return 0.0
+    params = {'query': place_name,
+              'key': KEY}
+    resp = requests.get(url, params=params)
+    data = resp.json()
+    import pdb; pdb.set_trace()
+    ratings = []
+    for place in data['results']:
+        resp2 = requests.get(
+            url_details,
+            params={'placeid': place['place_id'],
+                    'key': KEY}
+        )
+        data2 = resp2.json()
+        if 'rating' in data2['result']:
+            ratings.append(data2['result']['rating'])
+    if len(ratings) == 0:
+        return None
+    return sum(ratings) / len(ratings)
